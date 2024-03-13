b975d473ac0d81789bed63ff3ee5cac160a794fe
jjggu97 <jjggu@naver.com>
2024-03-14 02:31:23 +0900

    3/14

--- strawberry.py
+++ strawberry.py
@@ -5,10 +5,15 @@ CHANNEL_ID = '1217099488959664231'
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        channel = self.get_channel(int(CHANNEL_ID))
-        await channel.send('Strawberry')
+       print('Logged on as {0}!'.format(self.user))
+       await self.change_presence(status=discord.Status.online,
+ activity=discord.Game("strawberry"))
  
- 
+async def on_message(self,message):
+    if message.author == self.user:
+        return
+    
+
 intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
