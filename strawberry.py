d42f6e67ac68ef8e054f72675e590c0cad045b81
jjggu97 <jjggu@naver.com>
2024-03-16 20:59:20 +0900

    3/16

--- strawberry.py
+++ strawberry.py
@@ -1,18 +1,23 @@
 import discord
-
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client)) 
-
-@client.event
-async def on_message(message):
-    if message.author == client.user: 
-        return
-
-    if message.content.startswith('$hello'): 
-        await message.channel.send('Hello! 🍓') 
-
-# ADD YOUR TOKEN
-client.run('MTIxNzA5OTcyNjU4ODIxNTM3Ng.GlEu2_.Kj9eL-dElgLEn5JrT_2cZBBqfHzhAnJ2jF9ziA') 
+from discord.ext import commands
+ 
+DISCORD_CHANNEL_ID = 1217099488959664231
+DISCORD_BOT_TOKEN = "MTIxNzA5OTcyNjU4ODIxNTM3Ng.GRVm9L.8hvwcj2kgpoHLhsvHExzvCv1ckuD5bWf3PD2BM"
+ 
+if __name__ == "__main__":
+    intents = discord.Intents.default()
+    intents.message_content = True
+ 
+    bot = commands.Bot(command_prefix='!', intents=intents)
+ 
+    @bot.event
+    async def on_ready():
+        print(f'Bot is ready')
+        
+        channel = bot.get_channel(DISCORD_CHANNEL_ID)
+        if channel is None:
+            print('채널이 없어요 😂')
+            return
+        print('안녕하세요? 😊')
+    
+    bot.run(DISCORD_BOT_TOKEN)
