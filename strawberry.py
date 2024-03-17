1ecad66b7fcca6af3b7f81562a6e17283c592497
jjggu97 <jjggu@naver.com>
2024-03-18 01:02:51 +0900

    3/18

--- strawberry.py
+++ strawberry.py
@@ -1,24 +1,17 @@
 import discord
-from discord.ext import commands
- 
- 
-DISCORD_CHANNEL_ID = 1217099488959664231
-DISCORD_BOT_TOKEN = "MTIxNzA5OTcyNjU4ODIxNTM3Ng.GRVm9L.8hvwcj2kgpoHLhsvHExzvCv1ckuD5bWf3PD2BM"
- 
-if __name__ == "__main__":
-    intents = discord.Intents.default()
-    intents.message_content = True
- 
-    bot = commands.Bot(command_prefix='!', intents=intents)
- 
-    @bot.event
-    async def on_ready():
-        print(f'Bot is ready')
-        
-        channel = bot.get_channel(DISCORD_CHANNEL_ID)
-        if channel is None:
-            print('채널이 없어요 😂')
-            return
-        print('안녕하세요? 😊')
-    
-    bot.run(DISCORD_BOT_TOKEN)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == '안녕':
+        await message.channel.send('안녕하세요!')
+
+client.run('여러분의 디스코드 봇 토큰을 입력하세요')
